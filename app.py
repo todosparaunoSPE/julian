f353d9c68a43cc2ae7f4c303d4dd65beec0efb2a
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-05-30 15:17:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ import plotly.express as px
 # Título de la app
 st.title("🏥 Dashboard de Indicadores Clínicos - Hospital")
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Simulación de datos
 @st.cache_data
 
